87ff3df8f4d42e18521759af5b1aef75993c9ecd
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-03 09:09:57 +0545

    Halka

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,13 +1,10 @@
-// Querying with "sanityFetch" will keep content automatically updated
-// Before using it, import and render "<SanityLive />" in your layout, see
-// https://github.com/sanity-io/next-sanity#live-content-api for more information.
 import { defineLive } from "next-sanity";
-import { client } from './client'
+import { client } from "./client";
 
-export const { sanityFetch, SanityLive } = defineLive({ 
-  client: client.withConfig({ 
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX' 
-  }) 
+export const { sanityFetch, SanityLive } = defineLive({
+  client: client.withConfig({
+    apiVersion: "2024-03-01", // Use a valid API version
+  }),
+  serverToken: process.env.SANITY_SERVER_TOKEN, // Enables live drafts (optional)
+  browserToken: process.env.NEXT_PUBLIC_SANITY_BROWSER_TOKEN, // Enables client-side draft previews (optional)
 });
