94b1a05a5bdd029e6512da5815364be98a4395e9
Manish-Tamang <maneshtamang833@gmail.com>
2025-03-31 10:39:25 +0545

    03/31

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Karla } from "next/font/google"
 import "./globals.css"
 import { Navbar } from "@/components/ui/navbar"
 import { Footer } from "@/components/ui/footer"
 import { CartProvider } from "@/context/cart-context"
 
-
-const inter = Inter({
+const karla = Karla({
   subsets: ["latin"],
-  display: 'swap', // Optional: Improves performance 
+  weight: ["400", "600", "700"],
+  display: "swap"
 })
 
 export const metadata: Metadata = {
   title: "BlossomCart - Elegant Bouquets for Every Occasion",
-  description: "Discover the beauty of handpicked floral arrangements at BlossomCart. From romantic roses to vibrant mixed bouquets, we deliver fresh blooms for every celebration.",
+  description: "Discover the beauty of handpicked floral arrangements at BlossomCart. From romantic roses to vibrant mixed bouquets, we deliver fresh blooms for every celebration."
 }
 
 export default function RootLayout({
@@ -24,11 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={karla.className}>
         <CartProvider>
           <div className="flex min-h-screen flex-col">
             <Navbar />
-            <div className="max-w-4xl mx-auto" >
+            <div className="max-w-4xl mx-auto">
               <main className="flex-1">{children}</main>
             </div>
             <Footer />
